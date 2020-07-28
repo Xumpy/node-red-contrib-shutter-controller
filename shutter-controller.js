b591122fd91069fe78534b52a2fd86fcf3619d3c
nmartens <nico.martens@dhl.com>
2020-07-28 07:51:35 +0200

    First module shutter controller

---
+++ shutter-controller.js
@@ -0,0 +1,80 @@
+const Queue = require('sync-queue')
+
+function linkLogToConsole(context){
+    context.error = function(logMessage) {
+        const currentTime = new Date();
+        console.error(currentTime + ": " + logMessage);
+    }
+    context.debug = function(logMessage) {
+        const currentTime = new Date();
+        console.debug(currentTime + ": " + logMessage);
+    }
+    context.log = function(logMessage) {
+        const currentTime = new Date();
+        console.log(currentTime + ": " + logMessage);
+    }
+}
+
+module.exports = function(RED) {
+    const queue = Queue();
+    let currentPercentage = 0 // always assume that shutters are open at start of nodered;
+
+    function calculateTimeOut(config, differentPercentage){
+        let fullProcessingTime = Number(config.config.processTimeInSeconds);
+        let timeout = (fullProcessingTime / 100 * Math.abs(differentPercentage)) * 1000;
+        return timeout;
+    }
+
+    function sendUp(node, msg, timeOut){
+        queue.place( function() {
+            msg.payload = "UP";
+            node.send(msg);
+            setTimeout(function() {
+                queue.next();
+            }, timeOut);
+        });
+    }
+    function sendDown(node, msg, timeOut){
+        queue.place(function() {
+            msg.payload = "DOWN";
+            node.send(msg);
+            setTimeout(function() {
+                queue.next();
+            }, timeOut);
+        });
+    }
+    function sendStop(node, msg){
+        queue.place(function() {
+            msg.payload = "STOP";
+            node.send(msg); queue.next();
+        });
+    }
+
+    function shutterControllerNode(config) {
+        RED.nodes.createNode(this,config);
+
+        //linkLogToConsole(this);
+        var node = this;
+
+        node.on('input', function(msg) {
+            let shutterPercentage = Number(msg.payload.shutterPercentage);
+            let differentPercentage = shutterPercentage - currentPercentage;
+            // example 10% open, want to go to 50% needs to add 40% (40%)
+            // example 60% open, want to go to 50% needs to subtract 10% (-10%)
+
+            if (shutterPercentage === 0){
+                sendUp(node, msg, calculateTimeOut(config, differentPercentage));
+            } else if (shutterPercentage === 100){
+                sendDown(node, msg, calculateTimeOut(config, differentPercentage));
+            } else if (differentPercentage < 0){
+                sendUp(node, msg, calculateTimeOut(config, differentPercentage));
+                sendStop(node, msg);
+            } else if (differentPercentage > 0){
+                sendDown(node, msg, calculateTimeOut(config, differentPercentage));
+                sendStop(node, msg);
+            }
+            currentPercentage = shutterPercentage;
+        });
+    }
+    RED.nodes.registerType("shutter-controller",shutterControllerNode);
+}
---
+++ test/shutter-controller-test.js
@@ -0,0 +1,38 @@
+var helper = require("node-red-node-test-helper");
+var shutterControllerNode = require("../shutter-controller.js");
+
+helper.init(require.resolve('node-red'));
+
+describe('shutter-controller Node', function () {
+    this.timeout(30000);
+
+    afterEach(function () {
+        helper.unload();
+    });
+    it('Test shutter controller', function (done) {
+        var flow = [
+            { id: "n1", type: "shutter-controller", config: { idx: 1, processTimeInSeconds: 10 }, name: "shutter-controller",wires:[["n2"]] },
+            { id: "n2", type: "helper" }
+        ];
+
+        helper.load(shutterControllerNode, flow, function () {
+            var iterator = 0;
+            var n2 = helper.getNode("n2");
+            var n1 = helper.getNode("n1");
+            n2.on("input", function (msg) {
+                const currentTime = new Date();
+                console.log(currentTime + ": " + msg.payload);
+                iterator++; if (iterator === 5) done();
+            });
+            n1.receive({ payload: {
+                    shutterPercentage: "70"
+                } });
+            n1.receive({ payload: {
+                    shutterPercentage: "30"
+                } });
+            n1.receive({ payload: {
+                    shutterPercentage: "100"
+                } });
+        });
+    });
+});
